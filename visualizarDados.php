16e2d6b1002a7356e9bca95943fe9efd99a22f50
Lucas Emanuel <86731709+Lunmiel@users.noreply.github.com>
2021-09-17 22:20:34 -0300

    Update visualizarDados.php

--- visualizarDados.php
+++ visualizarDados.php
@@ -1,6 +1,13 @@
+<html>
+  <head>
+    <title>Dados do Usuário</title>
+  </head>
+  <body>
 <?php
     session_start();
     $nome = $_SESSION['nome'];
     $telefone = $_SESSION['telefone'];
     echo "Nome: $nome<br> Telefone: $telefone"
 ?>
+  </body>
+</html>
